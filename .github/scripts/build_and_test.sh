c187c35433247f2a7657b9392c646744a25c2c16
Basha Mougamadou <b.mougamadou@criteo.com>
2024-08-22 11:37:25 +0200

    fix build and test action
    Install docker-compose which appears to not being installed
    by default anymore.

--- .github/scripts/build_and_test.sh
+++ .github/scripts/build_and_test.sh
@@ -3,6 +3,8 @@
 set -x
 set -e
 
+sudo curl -Lo /usr/bin/docker-compose https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64
+sudo chmod 755 /usr/bin/docker-compose
 go run .github/scripts/prepare/main.go
 sleep 20
 
